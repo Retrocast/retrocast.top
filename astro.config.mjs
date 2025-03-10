bd9425010b2f26cb30fdc7d546d036b18144a093
Retrocast <retr0cast@proton.me>
2025-03-10 12:28:45 -0500

    chore: install Expressive Code

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import expressiveCode from 'astro-expressive-code';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://retrocast.top/',
   trailingSlash: 'always',
   output: 'static',
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   experimental: {
     contentIntellisense: true
-  }
+  },
+
+  integrations: [expressiveCode()]
 });
