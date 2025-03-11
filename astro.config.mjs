a468f2c530b8c1a6b1c29882cbe227c3d9fd4f08
Retrocast <retr0cast@proton.me>
2025-03-11 12:47:09 -0500

    chore: install rehype-autolink-headings & rehype-slug

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import theme from './src/styles/code-theme.json';
 
 import mdx from '@astrojs/mdx';
 
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://retrocast.top/',
@@ -20,6 +23,22 @@ export default defineConfig({
 
   experimental: {
     contentIntellisense: true
+  },
+
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'prepend',
+          content: {
+            type: 'text',
+            value: '#'
+          }
+        }
+      ]
+    ]
   },
 
   integrations: [expressiveCode({ themes: [theme] }), mdx()]
